4510442f991406a7511b60063137e6095afb6570
Kiiroihige <kiiroihigekaizoku@gmail.com>
2023-02-14 12:40:51 +0300

    lecture10.1.c

---
+++ lecture10.1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{  /*
+    //  LOOPS   :   FOR
+    int i;
+
+    for(i=1;i<=10;i++)
+    {
+        printf(" %d-  Hello World!\n",i);
+    }
+
+   */
+
+  int i;
+  //    LOOP COUNTER : i+=1     or  i+=2     or  i+=4   or   i+=10
+  for(i=2;i<=20;i+=2)
+  {
+    printf(" %d - Çift Sayısı\n",i);
+  }
+
+  return 0; 
+}
